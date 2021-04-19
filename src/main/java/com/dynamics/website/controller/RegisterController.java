1639f0c2c7b2230cc52db98f896b1386cc522025
akashdecoder <akash.ranjan1999@gmail.com>
2021-04-19 19:41:23 +0530

    Added pdf genration code for the recruitments page

--- src/main/java/com/dynamics/website/controller/RegisterController.java
+++ src/main/java/com/dynamics/website/controller/RegisterController.java
@@ -2,6 +2,18 @@ package com.dynamics.website.controller;
 
 import com.dynamics.website.model.AppUser;
 import com.dynamics.website.service.UserService;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1CFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.tomcat.jni.OS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,7 +24,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import javax.validation.Valid;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 @Controller
@@ -36,6 +53,13 @@ public class RegisterController {
             return "/dynamics";
         }
         userService.saveUser(appUser);
+
+        try {
+            generatePdf(appUser);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
         mailMessage.setFrom("dynamicsPOC19.sit@gmail.com");
@@ -49,7 +73,59 @@ public class RegisterController {
         mailMessage.setSubject(mailSubject);
         mailMessage.setText(mailContent);
 
+
         mailSender.send(mailMessage);
         return "registrationmessage.html";
+    }
+
+    public void generatePdf(AppUser user) throws IOException, DocumentException {
+        String OS = System.getProperty("os.name").toLowerCase();
+        String path = "";
+        if(OS.indexOf("win") >= 0) {
+            path = "D:/"+user.getFirstName()+".pdf";
+        } else if(OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") >= 0) {
+            path = "/home/" + user.getFirstName() + ".pdf";
+        }
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(path));
+        document.open();
+        addMetaData(document);
+        Paragraph paragraph = new Paragraph();
+        paragraph.add(new Paragraph("Registration", new Font(Font.FontFamily.TIMES_ROMAN, 18)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("FirstName: " + user.getFirstName(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("LastName: " + user.getLastName(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("Email: " + user.getEmail(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("Branch: " + user.getBranch(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("USN: " + user.getUsn(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("Year: " + user.getYear(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        paragraph.add(new Paragraph("Contact: " + user.getContact(), new Font(Font.FontFamily.TIMES_ROMAN, 12,
+                Font.BOLD)));
+        addEmptyLine(paragraph, 1);
+        document.add(paragraph);
+        document.newPage();
+        document.close();
+    }
+
+    public void addMetaData(Document document) {
+        document.addTitle("Registration Dynamics");
+    }
+
+    private static void addEmptyLine(Paragraph paragraph, int number) {
+        for (int i = 0; i < number; i++) {
+            paragraph.add(new Paragraph(" "));
+        }
     }
 }
