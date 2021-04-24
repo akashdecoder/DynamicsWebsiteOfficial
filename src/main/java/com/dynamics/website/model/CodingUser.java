0721a2ce0ba6c6f24a9af35f5b4cc987eb97d61a
akashdecoder <akash.ranjan1999@gmail.com>
2021-04-24 12:22:31 +0530

    Updated the some UI views

--- src/main/java/com/dynamics/website/model/CodingUser.java
+++ src/main/java/com/dynamics/website/model/CodingUser.java
@@ -21,10 +21,13 @@ public class CodingUser {
 
     private String event_name;
 
+    private String hack_id;
+
     public CodingUser() {
     }
 
-    public CodingUser(long coding_id, String firstName, String lastName, String year, String branch, String usn, String email, long contact, String event_name) {
+    public CodingUser(long coding_id, String firstName, String lastName, String year, String branch, String usn,
+                      String email, long contact, String event_name, String hack_id) {
         super();
         this.coding_id = coding_id;
         this.firstName = firstName;
@@ -35,6 +38,7 @@ public class CodingUser {
         this.email = email;
         this.contact = contact;
         this.event_name = event_name;
+        this.hack_id = hack_id;
     }
 
     public String getEvent_name() {
@@ -107,5 +111,13 @@ public class CodingUser {
 
     public void setContact(long contact) {
         this.contact = contact;
+    }
+
+    public String getHack_id() {
+        return hack_id;
+    }
+
+    public void setHack_id(String hack_id) {
+        this.hack_id = hack_id;
     }
 }
