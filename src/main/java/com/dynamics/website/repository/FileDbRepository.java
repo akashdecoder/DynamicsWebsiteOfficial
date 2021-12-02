5c44a10beb17221a8ffa5cccd883974ee8f40a76
akashdecoder <akash.ranjan1999@gmail.com>
2021-12-02 21:00:37 +0530

    Removed Files

--- src/main/java/com/dynamics/website/repository/FileDbRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.dynamics.website.repository;
-//
-//import com.dynamics.website.model.FileDb;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface FileDbRepository extends JpaRepository<FileDb, Long> {
-//}
--- src/main/java/com/dynamics/website/repository/FileUserRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.dynamics.website.repository;
-//
-//import com.dynamics.website.model.FileUser;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface FileUserRepository extends JpaRepository<FileUser, String> {
-//}
